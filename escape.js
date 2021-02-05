7f2043e94c61cfc95899c43ead5d04a490f5f1e1
Adrian Walker <awalker3365@gmail.com>
2021-02-04 22:05:21 -0500

    Update 1

---
+++ escape.js
@@ -0,0 +1,9 @@
+const readline =    require("readline-sync");
+const name = readline.question("What is your name? ");
+console.log('Hello ' + name + ' !');
+
+let endGame = false
+
+while(!endGame) {
+    const userAnswer = readline.keyInSelect
+}
